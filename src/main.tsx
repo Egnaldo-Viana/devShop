5ab439c97baa49e41051eb023694e6c6314e4a47
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2026-01-16 21:17:15 -0300

    feat: adicionando biblioteca toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { router } from './App.tsx';
 import { RouterProvider } from 'react-router';
+import { Toaster } from 'react-hot-toast';
 
 import CartProvider from './contexts/CartContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
+      <Toaster position="top-center" reverseOrder={false} />
       <RouterProvider router={router} />
     </CartProvider>
   </StrictMode>,
